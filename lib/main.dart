fb95672ba432ae39862e138d96684ae0b02340d8
Damanger <omar.cruzr97@hotmail.com>
2024-04-17 00:01:38 -0600

    adding search input

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,8 +32,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   LatLng? userLocation;
+  LatLng? searchLocation;
   bool showAdditionalButtons = false;
   final MapController mapController = MapController();
+  TextEditingController searchController = TextEditingController();
 
   Future<void> determineAndSetPosition() async {
     LocationPermission permission;
@@ -47,8 +50,37 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       userLocation = LatLng(position.latitude, position.longitude);
     });
-    // Forzar la actualización del mapa para centrarse en la nueva ubicación
     mapController.move(userLocation!, 10);
+  }
+
+  Future<void> searchAndMoveToPlace(String query) async {
+    List<Location> locations = await locationFromAddress(query);
+    if (locations.isNotEmpty) {
+      final LatLng newLocation =
+      LatLng(locations[0].latitude, locations[0].longitude);
+      setState(() {
+        searchLocation = newLocation;
+      });
+      mapController.move(newLocation, 10);
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('No se encontró ningún lugar con esta búsqueda.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
@@ -59,7 +91,10 @@ class _MyHomePageState extends State<MyHomePage> {
           height: 35,
           child: Text(
             'OpenStreetMap',
-            style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+            style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
           ),
         ),
         backgroundColor: Colors.blue,
@@ -92,7 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Stack(
       children: [
         FlutterMap(
-          mapController: mapController, // Añadir el controlador del mapa
+          mapController: mapController,
           options: MapOptions(
             center: userLocation!,
             zoom: 10,
@@ -101,21 +136,38 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           children: [
             openStreetMapTileLayer,
-            MarkerLayer(
-              markers: [
-                Marker(
-                  point: userLocation!,
-                  width: 60,
-                  height: 60,
-                  alignment: Alignment.centerLeft,
-                  child: const Icon(
-                    Icons.person_pin_circle_sharp,
-                    size: 60,
-                    color: Colors.blue,
+            if (userLocation != null)
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    point: userLocation!,
+                    width: 60,
+                    height: 60,
+                    alignment: Alignment.centerLeft,
+                    child: const Icon(
+                      Icons.person_pin_circle_sharp,
+                      size: 60,
+                      color: Colors.blue,
+                    ),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
+            if (searchLocation != null)
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    point: searchLocation!,
+                    width: 60,
+                    height: 60,
+                    alignment: Alignment.centerLeft,
+                    child: const Icon(
+                      Icons.location_pin,
+                      size: 60,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
           ],
         ),
         if (showAdditionalButtons)
@@ -126,7 +178,29 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 FloatingActionButton(
                   onPressed: () {
-                    // Lógica para el primer botón adicional
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Buscar ubicación'),
+                          content: TextField(
+                            controller: searchController,
+                            decoration: InputDecoration(
+                              hintText: 'Ingrese la ubicación',
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                searchAndMoveToPlace(searchController.text);
+                                Navigator.of(context).pop();
+                              },
+                              child: Text('Buscar'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
                   },
                   child: Icon(Icons.search),
                 ),
@@ -150,3 +224,4 @@ TileLayer get openStreetMapTileLayer => TileLayer(
   userAgentPackageName: 'dev.fleaflet.flutter_map.example',
   subdomains: const ['a', 'b', 'c'],
 );
+
