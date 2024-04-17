4feb201205e8e1dc46efebfffc293f78a2a34415
Damanger <omar.cruzr97@hotmail.com>
2024-04-16 20:42:48 -0600

    adding floating buttons

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
@@ -30,36 +31,40 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   LatLng? userLocation;
+  bool showAdditionalButtons = false;
+  final MapController mapController = MapController();
 
   Future<void> determineAndSetPosition() async {
-    final Position position = await determinePosition();
-    setState(() {
-      userLocation = LatLng(position.latitude, position.longitude);
-    });
-  }
-
-  Future<Position> determinePosition() async {
     LocationPermission permission;
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        throw 'error';
+        throw 'Location permission denied';
       }
     }
-    return await Geolocator.getCurrentPosition();
+    final Position position = await Geolocator.getCurrentPosition();
+    setState(() {
+      userLocation = LatLng(position.latitude, position.longitude);
+    });
+    // Forzar la actualización del mapa para centrarse en la nueva ubicación
+    mapController.move(userLocation!, 10);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Open Street Map',
-          style: TextStyle(fontSize: 22, color: Colors.white, fontWeight: FontWeight.bold),
+        title: const SizedBox(
+          height: 35,
+          child: Text(
+            'OpenStreetMap',
+            style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
         ),
         backgroundColor: Colors.blue,
         centerTitle: true,
+        toolbarHeight: 30,
       ),
       body: Center(
         child: userLocation == null
@@ -71,35 +76,70 @@ class _MyHomePageState extends State<MyHomePage> {
         )
             : content(),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            showAdditionalButtons = !showAdditionalButtons;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 
   Widget content() {
-    return FlutterMap(
-      options: MapOptions(
-        initialCenter: userLocation!,
-        initialZoom: 10,
-        maxZoom: 20,
-        minZoom: 1,
-        interactionOptions: const InteractionOptions(flags: ~InteractiveFlag.doubleTapDragZoom),
-      ),
+    return Stack(
       children: [
-        openStreetMapTileLayer,
-        MarkerLayer(
-          markers: [
-            Marker(
-              point: userLocation!,
-              width: 60,
-              height: 60,
-              alignment: Alignment.centerLeft,
-              child: const Icon(
-                Icons.person_pin_circle_sharp,
-                size: 60,
-                color: Colors.blue,
-              ),
+        FlutterMap(
+          mapController: mapController, // Añadir el controlador del mapa
+          options: MapOptions(
+            center: userLocation!,
+            zoom: 10,
+            maxZoom: 20,
+            minZoom: 1,
+          ),
+          children: [
+            openStreetMapTileLayer,
+            MarkerLayer(
+              markers: [
+                Marker(
+                  point: userLocation!,
+                  width: 60,
+                  height: 60,
+                  alignment: Alignment.centerLeft,
+                  child: const Icon(
+                    Icons.person_pin_circle_sharp,
+                    size: 60,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
+        if (showAdditionalButtons)
+          Positioned(
+            bottom: 120,
+            right: 16,
+            child: Column(
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    // Lógica para el primer botón adicional
+                  },
+                  child: Icon(Icons.search),
+                ),
+                SizedBox(height: 16),
+                FloatingActionButton(
+                  onPressed: () {
+                    determineAndSetPosition();
+                  },
+                  child: Icon(Icons.location_pin),
+                ),
+              ],
+            ),
+          ),
       ],
     );
   }
